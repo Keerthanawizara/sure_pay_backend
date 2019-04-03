b44772ee37bbe00453e653e4593dbb9f263fcfe8
WiZARA <keerthirajme@gmail.com>
2019-04-03 18:17:52 +0530

    on progress user module login api

--- src/users/userModel.js
+++ src/users/userModel.js
@@ -1,25 +1,4 @@
-// const dbConfig = require('../dbConfig')
-// const mongojs = require('mongojs')
 
-// class userDataModel {
-//     constructor(username, password) {
-//         console.log(this.db)
-//         this.db = mongojs(dbConfig.db)
-//         console.log(this.db)
-//         console.log(dbConfig.db)
-//         this.username = username
-//         console.log(this.username)
-//         this.password = password
-//     }
-//     findUser() {
-//         return new Promise((resolve,reject) => 
-//             this.db.collection("userCollection")
-//             .find({username: this.username, password: this.password},
-//             (err,docs) => err ? reject(err) : resolve(docs)))
-//     }
-// }
-
-// module.exports = userDataModel
 const mongoose = require('mongoose');
 
 //get the Schema class
