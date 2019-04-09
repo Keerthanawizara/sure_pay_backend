fcf4474877b629a5c3bae7ebfb30dafc3bd3cb0a
WiZARA <keerthirajme@gmail.com>
2019-04-09 07:56:11 +0530

    progress login

--- src/users/userController.js
+++ src/users/userController.js
@@ -25,60 +25,67 @@ const createUser = (req,h) => {
 }
 //GetUserList
 const GetUserList = (request,h) => {
-    return new Promise((resolve,reject) => 
+    return new Promise((resolve,reject) => {
         userCollection.paginate({},{offset:0, limit:15},(err,docs) => {
             if (err) {
            reject(err)
-           console.log(err)
+           //console.log(err)
              }else{
                resolve(docs)
              }
-        }))
+        })
+    })
 }
 
 
 //user login Api
 
 
-const userAuthController = (req,h) => {
-    const userCredentials = req.payload
-   // console.log(userCredentials)
-    return new Promise((resolve, reject)=> {
-        userCollection.find(userCredentials),((err,docs) => {
-            if(user[0]['_id']) {
+// const userAuthController = (req,h) => {
+//     const userCredentials = req.payload
+//     console.log(req.payload)
+//     console.log(userCredentials)
+//     return new Promise((resolve, reject)=> {
+//     userCollection.find(userCredentials),((err,docs) => {
+//             console.log(userCollection)
+//             if(user[0]['_id']) {
+//                 const userAuthToken = new userAuthentication()
+//                 console.log(userAuthToken)
+//                 userAuthToken.setToken(uuid())
+//                 return resolve ({ login: "success", token: userAuthToken.getToken() })
+//             }
+//             //else {
+//             //   console.log(err)
+//             //  // return reject(err)             
+//             // }
+//             if (err)
+//             return err
+//             else return(docs)
+
+
+//         })
+// })
+// }
+    
+
+const userAuthController = async (request) => {
+    const userCredentials = request.payload
+    console.log(userCredentials)
+        if (userCredentials.username && userCredentials.password) {
+            return new Promise((resolve, reject)=> {
+                userCollection.find(request.payload),((err,docs) => {
+            if (user[0]['_id']) {
                 const userAuthToken = new userAuthentication()
                 userAuthToken.setToken(uuid())
                 return resolve ({ login: "success", token: userAuthToken.getToken() })
-            }else {
-              return reject(err)             
+            } else {
+                return reject  ({ login: "failure", status: "user not available" })
             }
         })
-})
-}
+    })
+        }
     
-
-// const userAuthController = async (request) => {
-//     const userCredentials = request.payload
-//     try {
-//         if (userCredentials.username && userCredentials.password) {
-//             const userAuth = new userDataModel(userCredentials.username, userCredentials.password)
-//             const user = await userAuth.findUser()
-//             if (user[0]['_id']) {
-//                 const userAuthToken = new userAuthentication()
-//                 userAuthToken.setToken(uuid())
-//                 return { login: "success", token: userAuthToken.getToken() }
-//             } else {
-//                 return { login: "failure", status: "user not available" }
-//             }
-//         } else {
-//             return { login: "failure" }
-//         }
-//     } catch (e) {
-//         return e
-//     }
-// }
-
-
+    }
 
 module.exports = {
     createUser,
